420bb17ff36b30ce2215fbc356542561ade0a8c5
Santi-Mtz <santimtzh@gmail.com>
2026-08-12 01:23:01 -0600

    correccion final

--- Evaluacion_ll/telefono_app/android/app/src/main/kotlin/com/aegis/elden_telefono_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.aegis.elden_telefono_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Evaluacion_ll/wearable_app/android/app/src/main/kotlin/com/aegis/elden_wearable_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.aegis.elden_wearable_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
